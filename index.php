9c744aca4e55a46da0e592cf51163c75a544111b
Joshua Mormann <josh.mormann@gmail.com>
2014-02-13 10:34:59 -0500

    Refactor "order now" hero
    pulled the order now hero bar out, and created a partial, so edits can
    be made globally.

--- index.php
+++ index.php
@@ -152,27 +152,11 @@
 		</section>
 		<!-- End About 
 		============================================== -->
-		<!-- ==============================================
-		Hero Section
-		=============================================== -->
-		<section class="color-background hero-section">
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-8 hero-text">
-						<h2>Order now and get $25 your next Spartan Race!</h2>
-					</div>
-					<div class="col-sm-2">
-						<a href="http://www.spartanrace.com/spartan-up-amz" class="button-dark">Order on Amazon</a>
-					</div>
-					
-					<div class="col-sm-2">
-						<a href="http://www.spartanrace.com/spartan-up-bn" class="button-dark">Order on B&N</a>
-					</div>
-				</div>
-			</div>
-		</section>
-		<!-- End Hero Section 
-		============================================== -->		
+		
+		
+<?php require_once('partials/buysavehero.php'); ?>	
+		
+		
 		<!-- ==============================================
 		Testimonials
 		=============================================== -->
