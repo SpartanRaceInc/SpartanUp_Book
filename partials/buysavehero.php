4aa1a750e92058c79a5d2b226994250fecf968db
Joshua Mormann <josh.mormann@gmail.com>
2014-02-18 17:48:15 -0500

    Updates discount messaging

--- partials/buysavehero.php
+++ partials/buysavehero.php
@@ -5,7 +5,7 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-8 hero-text">
-						<h2>Order now and get $25 off your next Spartan Race!</h2>
+						<h2>Order now and get 15% off your next 2014 Spartan Race!</h2>
 					</div>
 					<div class="col-sm-2">
 						<a href="http://www.spartanrace.com/spartan-up-amz" class="button-dark">Order on Amazon</a>
