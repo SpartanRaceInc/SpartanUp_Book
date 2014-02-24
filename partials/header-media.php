160044f8b90b49e68693f36d40c60f49eff8f1f3
Joshua Mormann <josh.mormann@gmail.com>
2014-02-24 15:59:09 -0500

    Adds hidden Media outreach page to site

---
+++ partials/header-media.php
@@ -0,0 +1,48 @@
+	<body id="top">
+
+		<!-- ==============================================
+		Header
+		=============================================== -->
+		<section class="header">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+
+						<!-- ==============================================
+						Logo
+						=============================================== -->
+						<div class="logo float-left">
+							<a href="#top">
+								<img src="images/spartan.png" alt="logo-spartan" class="logo">
+							</a>
+						</div>
+						<!-- End Logo 
+						=============================================== -->
+
+						<!-- ==============================================
+						Navigation
+						=============================================== -->
+						<ul class="nav hidden-xs hidden-sm">
+							<li><a href="#ads">Ads</a></li>
+							<li><a href="#audio">Audio</a></li>
+							<li><a href="#blurbs">Blurbs</a></li>
+							<!-- <li><a href="#video">Video</a></li> -->
+						</ul>
+						<!-- End Navigation 
+						=============================================== -->
+
+						<!-- ==============================================
+						Mobile Navigation
+						=============================================== -->
+						<form name="nav" class="float-right">
+				         	<select class="mobile-nav hidden-md hidden-lg" data-autogenerate="true"></select>
+				        </form>
+				        <!-- End Mobile Navigation 
+						=============================================== -->
+
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Header 
+		=============================================== -->
