ee12938922fc4a656128d9eb3297ce823e71ef2c
Joshua Mormann <josh.mormann@gmail.com>
2014-02-25 14:15:22 -0500

    Adds Amazon affiliate link and pixel

--- partials/buysavehero.php
+++ partials/buysavehero.php
@@ -8,7 +8,7 @@
 						<h2>Order now and get 15% off your next 2014 Spartan Race!</h2>
 					</div>
 					<div class="col-sm-2">
-						<a href="http://www.spartanrace.com/spartan-up-amz" class="button-dark">Order on Amazon</a>
+						<a href="http://www.amazon.com/gp/product/0544286170/ref=as_li_tf_tl?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=0544286170&amp;linkCode=as2&amp;tag=spartanraceco-20" class="button-dark">Order on Amazon</a><img src="http://ir-na.amazon-adsystem.com/e/ir?t=spartanraceco-20&amp;l=as2&amp;o=1&amp;a=0544286170" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important; height:1px !important; width:1px !important;" />
 					</div>
 					
 					<div class="col-sm-2">
