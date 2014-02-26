5fd5c5edb7690c8b44d986f070356a8b68191fb4
Joshua Mormann <josh.mormann@gmail.com>
2014-02-26 10:39:29 -0500

    Adds Synopsis to Media Page, and refactors nav

--- media.php
+++ media.php
@@ -1,5 +1,53 @@
 <?php require_once('partials/fore.php'); ?>
-<?php require_once('partials/header-media.php'); ?>
+	<body id="top">
+
+		<!-- ==============================================
+		Header
+		=============================================== -->
+		<section class="header">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+
+						<!-- ==============================================
+						Logo
+						=============================================== -->
+						<div class="logo float-left">
+							<a href="#top">
+								<img src="images/spartan.png" alt="logo-spartan" class="logo">
+							</a>
+						</div>
+						<!-- End Logo 
+						=============================================== -->
+
+						<!-- ==============================================
+						Navigation
+						=============================================== -->
+						<ul class="nav hidden-xs hidden-sm">
+							<li><a href="#ads">Ads</a></li>
+							<li><a href="#audio">Audio</a></li>
+							<li><a href="#blurbs">Blurbs</a></li>
+							<li><a href="#texts">Texts</a></li>
+							<!-- <li><a href="#video">Video</a></li> -->
+						</ul>
+						<!-- End Navigation 
+						=============================================== -->
+
+						<!-- ==============================================
+						Mobile Navigation
+						=============================================== -->
+						<form name="nav" class="float-right">
+				         	<select class="mobile-nav hidden-md hidden-lg" data-autogenerate="true"></select>
+				        </form>
+				        <!-- End Mobile Navigation 
+						=============================================== -->
+
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Header 
+		=============================================== -->
 
 		<!-- ==============================================
 		Hero Section
@@ -179,6 +227,36 @@
 			</div>
 		</section>
 		<!-- End Testimonials
+		============================================== -->
+
+		<!-- ==============================================
+		Hero Section
+		=============================================== -->
+		<section class="color-background hero-section" id="texts">
+			<div class="container">
+
+				<div class="row">
+					<div class="col-sm-12">
+						<h2>Texts: </h2>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Hero Section 
+		============================================== -->		
+		<!-- ==============================================
+		About
+		=============================================== -->
+		<section class="light-gray-background audio">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h2 class="no-margin-bottom">Book Synopsis: <a href="pdf/BookSpartanUpSynopsis.pdf">BookSpartanUpSynopsis.pdf</a></h2>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End About 
 		============================================== -->
 
 <?php require_once('partials/footer.php'); ?>
