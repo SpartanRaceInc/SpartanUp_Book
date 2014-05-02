f334ff19ac7e34de4fdf10aafb2582ac55eced1d
Joshua Mormann <josh.mormann@gmail.com>
2014-05-02 18:35:05 -0400

    Adds volume purchasing incentives program page

---
+++ incentives.php
@@ -0,0 +1,356 @@
+<?php
+	$pageTitle = 'Bonus Book Deals';
+	require_once('partials/fore.php');
+?>
+<?php require_once('partials/header.php'); ?>
+
+<!-- ==============================================
+		Title Area
+		=============================================== -->
+		<section class="dark-gray-background hero-section" id="stinger">
+			<div class="container">
+
+				<div class="row">
+					<div class="col-sm-12">
+						<h2>Bonus Book Deals!</h2>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Title Area
+		============================================== -->
+
+
+		
+		<!--First Deal
+		=============================================== -->
+		<!-- ==============================================
+		Hero Section
+		=============================================== -->
+		<section class="white-background">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 hero-text">
+						<h2>Purchase 2 Books</h2>
+					</div>
+					<div class="col-sm-4">
+						<a href="mailto:enteryour@addresshere.com?subject=Two Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! <br> - Spartan HQ" class="button-light">Claim Deal</a>
+					</div>
+				</div>
+
+
+				<div class="row">
+					<!--First Deal
+					=============================================== -->
+					<div class="col-md-12">
+						<ul class="info">
+Earn a 50% off discount to any 2014 Domestic Spartan Race event -  <strong>$60 value, limit 150 participants</strong>
+						</ul>
+					</div>
+					<!-- End First Deal 
+					============================================== -->
+				</div>
+			</div>
+		</section>
+
+		<!-- End Hero Section 
+		============================================== -->	
+
+	
+
+		<!-- End First Deal 
+		============================================== -->
+
+
+        <!--Second Deal
+		=============================================== -->
+		<!-- ==============================================
+		Hero Section
+		=============================================== -->
+		<section class="color-background hero-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 hero-text">
+						<h2>Purchase 5 Books</h2>
+					</div>
+					<div class="col-sm-4">
+						<a href="mailto:enteryour@addresshere.com?subject=Five Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! <br> - Spartan HQ" class="button-dark">Claim Deal</a>
+					</div>	
+				</div>
+
+
+				<div class="row">
+					<div class="col-md-12">
+						<ul class="info">
+Earn one FREE entry for you and a guest to any 2014 Domestic Spartan Race Event - <strong>$200 value, tickets to be used on the same day, limit 75 participants</strong>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Hero Section 
+		============================================== -->			
+		<!-- End Second Deal 
+		============================================== -->
+
+
+        <!--Third Deal
+		=============================================== -->
+		<!-- ==============================================
+		Hero Section
+		=============================================== -->
+		<section class="white-background">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 hero-text">
+						<h2>Purchase 10 Books</h2>
+					</div>
+					<div class="col-sm-4">
+						<a href="mailto:enteryour@addresshere.com?subject=Ten Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! <br> - Spartan HQ" class="button-light">Claim Deal</a>
+					</div>
+				</div>
+
+
+				<div class="row">
+					<div class="col-md-12">
+						<ul class="info">
+FREE Season Pass for 2015 Domestic Spartan Races - <strong>$475 value, limit 25 participants</strong>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Hero Section 
+		============================================== -->		
+		<!-- End Third Deal 
+		============================================== -->
+
+        <!--Fourth Deal
+		=============================================== -->
+		<!-- ==============================================
+		Hero Section
+		=============================================== -->
+		<section class="color-background hero-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 hero-text">
+						<h2>Purchase 25 Books</h2>
+					</div>
+					<div class="col-sm-4">
+						<a href="mailto:enteryour@addresshere.com?subject=Twenty-Five Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! <br> - Spartan HQ" class="button-dark">Claim Deal</a>
+					</div>
+				</div>
+
+
+				<div class="row">
+					<div class="col-md-12">
+						<ul class="info">
+FREE entry to the Spartan Race World Championships in Killington, Vermont on 9/20/14, 2 Night stay at Spartan HQ Bed & Breakfast (subject to availability), FREE pair of Reebok Spartan Race shoes, and a FREE Season Pass for 2015 Domestic Spartan Races - <strong>$1070 value, limit to 10 participants</strong>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Hero Section 
+		============================================== -->		
+		<!-- End Fourth Deal 
+		============================================== -->
+
+
+        <!--Fifth Deal
+		=============================================== -->
+		<!-- ==============================================
+		Hero Section
+		=============================================== -->
+		<section class="white-background">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 hero-text">
+						<h2>Purchase 50 Books</h2>
+					</div>
+					<div class="col-sm-4">
+						<a href="mailto:enteryour@addresshere.com?subject=Fifty Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! <br> - Spartan HQ" class="button-light">Claim Deal</a>
+					</div>
+				</div>
+
+
+				<div class="row">
+					<div class="col-md-12">
+						<ul class="info">
+FREE 2015 Season Pass to ALL Spartan Races, 3 night stay at Spartan HQ Bed & Breakfast for you and a guest, dinner with Spartan founder Joe Desena, one FREE training session at Spartan HQ, and one FREE cooking session with a 5-Star Spartan Chef - <strong>$1910 value, limit to 5 participants</strong>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Hero Section 
+		============================================== -->		
+		<!-- End Fifth Deal 
+		============================================== -->
+
+
+        <!--Sixth Deal
+		=============================================== -->
+		<!-- ==============================================
+		Hero Section
+		=============================================== -->
+		<section class="color-background hero-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 hero-text">
+						<h2>Purchase 100 Books</h2>
+					</div>
+					<div class="col-sm-4">
+						<a href="mailto:enteryour@addresshere.com?subject=Hundred Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! - Spartan HQ" class="button-dark">Claim Deal</a>
+					</div>
+				</div>
+
+
+				<div class="row">
+					<div class="col-md-12">
+						<ul class="info">
+5-night stay at Spartan HQ Bed & Breakfast, $1,000 gift card to Bay Ridge Bikes, LIFETIME pass to Domestic Spartan Races, Dinner with Spartan founder Joe Desena at Spartan HQ, one FREE registration for a Spartan Group X Coaching Certification - <strong>$12,200 value, limit to 4 participants</strong>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Hero Section 
+		============================================== -->		
+		<!-- End Sixth Deal 
+		============================================== -->
+
+
+         <!--Seventh Deal
+		=============================================== -->
+		<!-- ==============================================
+		Hero Section
+		=============================================== -->
+		<section class="white-background">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 hero-text">
+						<h2>Purchase 250 Books</h2>
+					</div>
+					<div class="col-sm-4">
+						<a href="mailto:enteryour@addresshere.com?subject=Two Hundred Fifty Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! - Spartan HQ" class="button-light">Claim Deal</a>
+					</div>
+				</div>
+
+
+				<div class="row">
+					<div class="col-md-12">
+						<ul class="info">
+LIFETIME Pass to ALL Domestic and International Spartan Races for you and a guest, 5-night stay at Spartan HQ Bed & Breakfast, 1 day of Survival Training with Former Special Forces Leader, 1 training session with Spartan Founder Joe Desena, dinner with Spartan Founder Joe Desena - <strong>$26,725 value, limited to 3 participants</strong>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Hero Section 
+		============================================== -->		
+		<!-- End Seventh Deal 
+		============================================== -->
+
+
+         <!--Eighth Deal
+		=============================================== -->
+		<!-- ==============================================
+		Hero Section
+		=============================================== -->
+		<section class="color-background hero-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 hero-text">
+						<h2>Purchase 500 Books</h2>
+					</div>
+					<div class="col-sm-4">
+						<a href="mailto:enteryour@addresshere.com?subject=Five Hundred Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! - Spartan HQ" class="button-dark">Claim Deal</a>
+					</div>
+				</div>
+
+
+				<div class="row">
+					<div class="col-md-12">
+						<ul class="info">
+Joe will travel to and speak to your company for 90 minutes and also create a presentation according to your company needs  - <strong>$30,000 value, unlimited participants</strong>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Hero Section 
+		============================================== -->		
+		<!-- End Eighth Deal 
+		============================================== -->
+
+
+         <!--Ninth Deal
+		=============================================== -->
+		<!-- ==============================================
+		Hero Section
+		=============================================== -->
+		<section class="white-background">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 hero-text">
+						<h2>Purchase 1000 Books</h2>
+					</div>
+					<div class="col-sm-4">
+						<a href="mailto:enteryour@addresshere.com?subject=One Thousand Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! - Spartan HQ" class="button-light">Claim Deal</a>
+					</div>
+				</div>
+
+
+				<div class="row">
+					<div class="col-md-12">
+						<ul class="info">
+Spend a week with Joe & The Spartan Team at Spartan HQ, fitness, training, meals and more, ALL travel expenses covered. Also experience an entire summer apprenticeship to work with a 5-Star Chef at Spartan HQ and leave as a certified 5-Star Chef. Your entire summer stay will be covered, meals included - <strong>$50,000 value, limited to 3 participants</strong>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Hero Section 
+		============================================== -->		
+		<!-- End Ninth Deal 
+		============================================== -->
+
+
+        <!--Tenth Deal
+		=============================================== -->
+		<!-- ==============================================
+		Hero Section
+		=============================================== -->
+		<section class="color-background hero-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 hero-text">
+						<h2>Purchase 10,000 Books</h2>
+					</div>
+					<div class="col-sm-4">
+						<a href="mailto:enteryour@addresshere.com?subject=Ten Thousand Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! - Spartan HQ" class="button-dark">Claim Deal</a>
+					</div>
+				</div>
+
+
+				<div class="row">
+					<div class="col-md-12">
+						<ul class="info">
+You will be set up with a guide and 3 guests of your choice to climb Mount Everest. All gear and expenses are fully covered. Also experience an entire summer stay at Spartan HQ, all expenses covered. Spend the summer with Joe and the Spartan team and live the life of a true Spartan - <strong>$300,000 value, limited to 1 participant</strong>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Hero Section 
+		============================================== -->		
+		<!-- End Tenth Deal 
+		============================================== -->
+					
+
+					
+
+
+<?php require_once('partials/footer.php'); ?>
+<?php require_once('partials/aft.php'); ?>
