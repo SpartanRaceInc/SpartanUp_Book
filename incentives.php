605a415936bee8728b6dd078afffab7c4ca84cd9
Joshua Mormann <josh.mormann@gmail.com>
2014-05-07 18:59:42 -0400

    adds email addresses to the

--- incentives.php
+++ incentives.php
@@ -34,7 +34,7 @@
 						<h2>Purchase 2 Books</h2>
 					</div>
 					<div class="col-sm-4">
-						<a href="mailto:enteryour@addresshere.com?subject=Two Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! <br> - Spartan HQ" class="button-light">Claim Deal</a>
+						<a href="mailto:spartanuplevel1@spartan.com?subject=Two Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! <br> - Spartan HQ" class="button-light">Claim Deal</a>
 					</div>
 				</div>
 
@@ -74,7 +74,7 @@ Earn a 50% off discount to any 2014 Domestic Spartan Race event -  <strong>$60 v
 						<h2>Purchase 5 Books</h2>
 					</div>
 					<div class="col-sm-4">
-						<a href="mailto:enteryour@addresshere.com?subject=Five Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! <br> - Spartan HQ" class="button-dark">Claim Deal</a>
+						<a href="mailto:spartanuplevel2@spartan.com?subject=Five Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! <br> - Spartan HQ" class="button-dark">Claim Deal</a>
 					</div>	
 				</div>
 
@@ -106,7 +106,7 @@ Earn one FREE entry for you and a guest to any 2014 Domestic Spartan Race Event 
 						<h2>Purchase 10 Books</h2>
 					</div>
 					<div class="col-sm-4">
-						<a href="mailto:enteryour@addresshere.com?subject=Ten Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! <br> - Spartan HQ" class="button-light">Claim Deal</a>
+						<a href="mailto:spartanuplevel3@spartan.com?subject=Ten Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! <br> - Spartan HQ" class="button-light">Claim Deal</a>
 					</div>
 				</div>
 
@@ -137,7 +137,7 @@ FREE Season Pass for 2015 Domestic Spartan Races - <strong>$475 value, limit 25 
 						<h2>Purchase 25 Books</h2>
 					</div>
 					<div class="col-sm-4">
-						<a href="mailto:enteryour@addresshere.com?subject=Twenty-Five Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! <br> - Spartan HQ" class="button-dark">Claim Deal</a>
+						<a href="mailto:spartanuplevel4@spartan.com?subject=Twenty-Five Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! <br> - Spartan HQ" class="button-dark">Claim Deal</a>
 					</div>
 				</div>
 
@@ -169,7 +169,7 @@ FREE entry to the Spartan Race World Championships in Killington, Vermont on 9/2
 						<h2>Purchase 50 Books</h2>
 					</div>
 					<div class="col-sm-4">
-						<a href="mailto:enteryour@addresshere.com?subject=Fifty Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! <br> - Spartan HQ" class="button-light">Claim Deal</a>
+						<a href="mailto:spartanuplevel5@spartan.com?subject=Fifty Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! <br> - Spartan HQ" class="button-light">Claim Deal</a>
 					</div>
 				</div>
 
@@ -201,7 +201,7 @@ FREE 2015 Season Pass to ALL Spartan Races, 3 night stay at Spartan HQ Bed & Bre
 						<h2>Purchase 100 Books</h2>
 					</div>
 					<div class="col-sm-4">
-						<a href="mailto:enteryour@addresshere.com?subject=Hundred Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! - Spartan HQ" class="button-dark">Claim Deal</a>
+						<a href="mailto:spartanuplevel6@spartan.com?subject=Hundred Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! - Spartan HQ" class="button-dark">Claim Deal</a>
 					</div>
 				</div>
 
@@ -233,7 +233,7 @@ FREE 2015 Season Pass to ALL Spartan Races, 3 night stay at Spartan HQ Bed & Bre
 						<h2>Purchase 250 Books</h2>
 					</div>
 					<div class="col-sm-4">
-						<a href="mailto:enteryour@addresshere.com?subject=Two Hundred Fifty Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! - Spartan HQ" class="button-light">Claim Deal</a>
+						<a href="mailto:spartanuplevel7@spartan.com?subject=Two Hundred Fifty Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! - Spartan HQ" class="button-light">Claim Deal</a>
 					</div>
 				</div>
 
@@ -265,7 +265,7 @@ LIFETIME Pass to ALL Domestic and International Spartan Races for you and a gues
 						<h2>Purchase 500 Books</h2>
 					</div>
 					<div class="col-sm-4">
-						<a href="mailto:enteryour@addresshere.com?subject=Five Hundred Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! - Spartan HQ" class="button-dark">Claim Deal</a>
+						<a href="mailto:spartanuplevel8@spartan.com?subject=Five Hundred Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! - Spartan HQ" class="button-dark">Claim Deal</a>
 					</div>
 				</div>
 
@@ -297,7 +297,7 @@ Joe will travel to and speak to your company for 90 minutes and also create a pr
 						<h2>Purchase 1000 Books</h2>
 					</div>
 					<div class="col-sm-4">
-						<a href="mailto:enteryour@addresshere.com?subject=One Thousand Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! - Spartan HQ" class="button-light">Claim Deal</a>
+						<a href="mailto:spartanuplevel9@spartan.com?subject=One Thousand Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! - Spartan HQ" class="button-light">Claim Deal</a>
 					</div>
 				</div>
 
@@ -329,7 +329,7 @@ Spend a week with Joe & The Spartan Team at Spartan HQ, fitness, training, meals
 						<h2>Purchase 10,000 Books</h2>
 					</div>
 					<div class="col-sm-4">
-						<a href="mailto:enteryour@addresshere.com?subject=Ten Thousand Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! - Spartan HQ" class="button-dark">Claim Deal</a>
+						<a href="mailto:spartanuplevel10@spartan.com?subject=Ten Thousand Book Purchase Incentive&body=Please attach a scan or saved copy of your receipt, as well as your name, address and contact information! Thank you! - Spartan HQ" class="button-dark">Claim Deal</a>
 					</div>
 				</div>
 
