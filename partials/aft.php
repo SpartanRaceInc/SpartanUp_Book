e5d2231b038be3cc0c0b6f9fea9feec6e2097177
Joshua Mormann <josh.mormann@gmail.com>
2014-10-22 16:19:19 -0400

    adds GTM code to the microsite

--- partials/aft.php
+++ partials/aft.php
@@ -46,16 +46,14 @@
 		<!-- Amazon Tracking Pixel: -->
 		<img src="http://ir-na.amazon-adsystem.com/e/ir?t=spartanraceco-20&amp;l=as2&amp;o=1&amp;a=0544286170" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important; height:1px !important; width:1px !important;" />
 
-		<!-- GA Tracking Code: -->
-		<script>
-		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		
-		  ga('create', 'UA-8299720-49', 'spartanupthebook.com');
-		  ga('send', 'pageview');
-		
-		</script>
+		<!-- Book: Google Tag Manager -->
+		<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PLR8GK"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-PLR8GK');</script>
+		<!-- End Google Tag Manager -->
 	</body>
 </html>
